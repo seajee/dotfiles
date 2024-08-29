12cba85d5c2fb9645ea620f9622bcc78eaedb760
seajee <teapods@proton.me>
2024-08-29 04:12:21 +0200

    Evil eval

--- update.sh
+++ update.sh
@@ -5,5 +5,5 @@ files=$(find . -mindepth 1 -type f)
 cd ..
 
 while IFS= read -r filepath; do
-    eval cp "~/$filepath" "./home/$filepath"
+    cp "$HOME/$filepath" "./home/$filepath"
 done <<< "$files"
