eefd47cdffd76409550e8bb4842cf4128a5edea9
seajee <teapods@proton.me>
2024-08-29 03:51:54 +0200

    Improve install script

--- install.sh
+++ install.sh
@@ -1,3 +1,31 @@
 #!/usr/bin/env bash
 
-cp -r -i ./home/.* ~/
+cd ./home/
+files=$(find . -mindepth 1 -type f)
+directories=$(find . -mindepth 1 -type d)
+cd ..
+
+while IFS= read -r directory; do
+    mkdir -p "./testing/$directory"
+done <<< "$directories"
+
+readarray -t files_array <<< "$files"
+
+for filepath in "${files_array[@]}"
+do
+    source_path="./home/$filepath"
+    target_path="$HOME/$filepath"
+
+    if [ -f "$target_path" ]; then
+        if ! cmp -s "$source_path" "$target_path"; then
+            # file=$(basename "$filepath")
+            read -p "overwrite ${target_path}? [y/N] " input
+            if [[ "$input" == [Yy]* ]]; then
+                cp "$source_path" "$target_path"
+            fi
+        fi
+    else
+        cp "$source_path" "$target_path"
+    fi
+done
+
