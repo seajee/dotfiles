85a8541dc8900e391b855b6b1d5753a9f9043b2f
seajee <teapods@proton.me>
2025-02-26 20:10:55 +0100

    [neovim] Change colorscheme

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -226,31 +226,9 @@ require("lazy").setup({
 
     -- Colorscheme
     {
-        "Yagua/nebulous.nvim",
+        "ellisonleao/gruvbox.nvim",
         config = function()
-            require("nebulous").setup({
-                variant = "fullmoon",
-                disable = {
-                    background = true,
-                    endOfBuffer = false,
-                    terminal_colors = true,
-                },
-                italic = {
-                    comments = false,
-                    keywords = true,
-                    functions = false,
-                    variables = false,
-                },
-                custom_colors = { -- this table can hold any group of colors with their respective values
-                    LineNr = { fg = "#5BBBDA", bg = "NONE", style = "NONE" },
-                    CursorLineNr = { fg = "#E1CD6C", bg = "NONE", style = "NONE" },
-
-                    -- it is possible to specify only the element to be changed
-                    TelescopePreviewBorder = { fg = "#A13413" },
-                    LspDiagnosticsDefaultError = { bg = "#E11313" },
-                    TSTagDelimiter = { style = "bold,italic" },
-                }
-            })
+            vim.cmd.colorscheme("gruvbox")
         end
     },
 
@@ -283,7 +261,7 @@ require("lazy").setup({
 
             configs.setup({
                 sync_install = false,
-                highlight = { enable = true },
+                highlight = { enable = false },
                 indent = { enable = true },
             })
         end
