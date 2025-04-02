e445249fc5aa1ce67fdb068d8431280b39f30e9b
seajee <teapods@proton.me>
2025-04-02 15:04:43 +0200

    [neovim] Reconfigure diagnostics keymaps

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -138,7 +138,7 @@ vim.keymap.set("n", "<leader>\\", "<cmd>b term<CR>")
 -- Diagnostic keymaps
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+vim.keymap.set("n", "L", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
@@ -285,8 +285,8 @@ require("lazy").setup({
 
             configs.setup({
                 sync_install = false,
-                highlight = { enable = false },
-                indent = { enable = true },
+                highlight = { enable = true },
+                indent = { enable = false },
             })
         end
     },
