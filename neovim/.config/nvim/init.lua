14a7a6fb4530204a3a589cfaef6fcffb21cae610
seajee <teapods@proton.me>
2025-04-05 16:08:20 +0200

    [neovim] Change colorscheme

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -228,31 +228,10 @@ require("lazy").setup({
 
     -- Colorscheme
     {
-        "Yagua/nebulous.nvim",
+        "savq/melange-nvim",
         config = function()
-            require("nebulous").setup({
-                variant = "fullmoon",
-                disable = {
-                    background = false,
-                    endOfBuffer = false,
-                    terminal_colors = true,
-                },
-                italic = {
-                    comments = false,
-                    keywords = true,
-                    functions = false,
-                    variables = false,
-                },
-                custom_colors = { -- this table can hold any group of colors with their respective values
-                    LineNr = { fg = "#5BBBDA", bg = "NONE", style = "NONE" },
-                    CursorLineNr = { fg = "#E1CD6C", bg = "NONE", style = "NONE" },
-
-                    -- it is possible to specify only the element to be changed
-                    TelescopePreviewBorder = { fg = "#A13413" },
-                    LspDiagnosticsDefaultError = { bg = "#E11313" },
-                    TSTagDelimiter = { style = "bold,italic" },
-                }
-            })
+            vim.opt.termguicolors = true
+            vim.cmd.colorscheme("melange")
         end
     },
 
